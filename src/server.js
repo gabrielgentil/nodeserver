db8ca0f7d128f3e95684cf3a54cc8b874e05630f
gabrielgentil <gabriel.sgentil97@gmail.com>
2022-06-11 15:19:05 -0300

    chore: add dockerfile, docker-compose and nodemon

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 // Arquivo server.js
 const http = require('http')
-const host = 'localhost'
-const port = 8000
+const host = '0.0.0.0'
+const port = 3000
 const requestListener = function (req, res) {
 	console.log('request received')
 	res.setHeader('Content-Type', 'text/html')
 	res.writeHead(200)
-	res.end('<img src="https://pbs.twimg.com/media/E3yyK-dWYAIsxMn.jpg">')
+	res.end(
+		'<div style="padding-top:25vh ;width: 700px; margin-left: auto; margin-right: auto;"><h1>Nove horas vamos embora !</h1><img src="https://pbs.twimg.com/media/E3yyK-dWYAIsxMn.jpg"> </div>'
+	)
 }
 const server = http.createServer(requestListener)
 server.listen(port, host, () => {
